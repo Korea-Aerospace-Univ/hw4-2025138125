5fa299e2daf76cdf3854b4bbb32254fb960e407d
2025138125 <chanhee2018@naver.com>
2026-04-15 21:07:28 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -2,22 +2,22 @@
 
 int main()
 {
- int ans =0, n =0, count = 0;
- scanf("%d", &ans);
+ int ans =0, n =0, count = 0;//변수 선언 및 초기화
+ scanf("%d", &ans); //정답 입력 받기
  do {
- scanf("%d", &n);
+ scanf("%d", &n);//도전할 수 받기
  if (ans < n){
     printf("%d>?\n", n); 
- }
+ }//입력받는 값이 정답보다 클때
  else if(ans > n) {
      printf("%d<?\n", n);
- }
+ }//입력받는 값이 정답보다 작을때
  else {
      printf("%d==?\n", n);
- }
- count ++;
-} while(ans != n);
+ }//둘이 같을때
+ count ++;//한번 돌때마다 시행 횟수체크
+} while(ans != n); //언제까지 반복할지
 
-printf("%d", count);
+printf("%d", count);//시행횟수 출력
 return 0;
 }
