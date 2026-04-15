42203016bd4f66c87306bbdfc597b94411e3055b
2025138125 <chanhee2018@naver.com>
2026-04-15 15:25:29 +0900

    Update main.c

---
+++ p1/main.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+
+int main()
+{
+ int ans =0, n =0, count = 0;
+ scanf("%d", &ans);
+ do {
+ scanf("%d", &n);
+ if (ans > n){
+    printf("%d > %d\n", ans, n); 
+ }
+ else if(ans < n) {
+     printf("%d < %d\n", ans, n);
+ }
+ else {
+     printf("%d == %d\n", ans, n);
+ }
+ count ++;
+} while(ans != n);
+
+printf("시도횟수는 %d회", count);
+return 0;
+}
