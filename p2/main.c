c5df56736e43698aef20d17473ecd18e6e67663c
2025138125 <chanhee2018@naver.com>
2026-04-15 16:27:36 +0900

    Update main.c

---
+++ p2/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+ int n=0;
+ int eng = 0, num =0;
+ int max_num =0, max_eng =0;
+ char a = '\0';
+ scanf("%d", &n);
+ for(int i =0; i<=n;i++) {
+     scanf("%c", &a);
+     if('a'<= a && a <= 'z'){
+         eng++;
+         num =0;
+     } 
+     else if('0' <= a && a <= '9') {
+         num++;
+         eng = 0;
+     }
+     else {
+         continue;
+     }
+     
+     if(eng !=0) {
+         max_eng =eng;
+     }
+     if(num !=0) {
+         max_num =num;
+     }
+ }
+ printf("%d\n%d",max_eng, max_num);
+ return 0;
+ 
+}
